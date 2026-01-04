f8f06eec3bde8e287124d60ee3e586086a963e8d
Karly Nelson <karly.nelson@hey.com>
2026-01-04 16:13:53 -0600

    ch3 l1

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from stats import get_num_words, get_book_text, get_character_count
+from stats import get_num_words, get_book_text, get_character_count_map, get_sorted_character_count
 
 def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(text)
-    num_characters = get_character_count(text)
+    character_map = get_character_count_map(text)
+    characters_sorted = get_sorted_character_count(character_map)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(num_characters)
+    print("--------- Character Count -------")
+    for item in characters_sorted:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 main()
 
--- stats.py
+++ stats.py
@@ -8,12 +8,26 @@ def get_num_words(text):
     return len(words)
     
 
-def get_character_count(text):
+def get_character_count_map(text):
     characters = text.lower()
     character_count = {}
     for character in characters:
+        if character.isalpha() == False:
+            continue  
         if character in character_count:
             character_count[character] += 1
         else:
             character_count[character] = 1
     return character_count
+
+def get_sorted_character_count(characters_map):
+    def sort_on(items):
+      return items["num"]
+    
+    sorted_characters = []
+    for character in characters_map:
+        sorted_characters.append({"char": character, "num": characters_map[character]})
+    
+    sorted_characters.sort(reverse=True, key=sort_on)
+
+    return sorted_characters
