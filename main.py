b414665261dff9f57f00f70d28178e2b92fd2b08
Karly Nelson <karly.nelson@hey.com>
2026-01-04 16:20:05 -0600

    ch3 l2

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import sys
 from stats import get_num_words, get_book_text, get_character_count_map, get_sorted_character_count
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+      print("Usage: python3 main.py <path_to_book>")
+      sys.exit(1)
+      return
+    
+    book_path = sys.argv[1] 
+    text = get_book_text(book_path)
     num_words = get_num_words(text)
     character_map = get_character_count_map(text)
     characters_sorted = get_sorted_character_count(character_map)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
